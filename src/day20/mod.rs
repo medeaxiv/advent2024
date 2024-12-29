299b38604e31e8d03916280af88945250ad57d8c
medeaxiv <105075687+medeaxiv@users.noreply.github.com>
2024-12-29 10:56:12 -0500

    Day 20 - Only store walkable tiles

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -1,9 +1,7 @@
-use std::collections::VecDeque;
-
 use allocator_api2::vec::Vec;
 use aoc_utils::{
-    grid::{Coordinates, Grid},
-    hashbrown::HashSet,
+    grid::Coordinates,
+    hashbrown::{HashMap, HashSet},
     neighbors::CardinalNeighbors,
     numerics::manhattan_distance,
     AocError,
@@ -26,41 +24,20 @@ fn part_1(input: &str, threshold: i64) -> anyhow::Result<u64> {
 }
 
 fn count_shortcuts(map: &Map, threshold: i64, limit: u32) -> u64 {
-    let mut queue = VecDeque::new();
-    let mut visited = HashSet::new();
-
     let mut count = 0;
-    for &shortcut_start in map.path.iter() {
-        let Tile::Path(start_distance) = map.grid[shortcut_start] else {
-            continue;
-        };
-
-        queue.push_back(shortcut_start);
-        while let Some(node) = queue.pop_front() {
-            if !visited.insert(node) {
+    for (&shortcut_start, start_distance) in map.tiles.iter() {
+        for (&node, &end_distance) in map.tiles.iter() {
+            let shortcut_len = manhattan_distance(shortcut_start, node);
+            if shortcut_len > limit {
                 continue;
             }
 
-            let shortcut_len = manhattan_distance(shortcut_start, node);
-            if shortcut_len > limit {
-                break;
-            }
-
-            if let Some(&Tile::Path(end_distance)) = map.grid.get(node) {
-                let shortcut_distance = end_distance - start_distance - shortcut_len as i64;
-                if shortcut_distance >= threshold {
-                    // tracing::debug!(?shortcut_start, ?node, shortcut_distance);
-                    count += 1;
-                }
-            }
-
-            for neighbor in CardinalNeighbors::new(node) {
-                queue.push_back(neighbor);
+            let shortcut_distance = end_distance - start_distance - shortcut_len as i64;
+            if shortcut_distance >= threshold {
+                // tracing::debug!(?shortcut_start, ?node, shortcut_distance);
+                count += 1;
             }
         }
-
-        queue.clear();
-        visited.clear();
     }
 
     count
@@ -73,98 +50,57 @@ fn part_2(input: &str, threshold: i64) -> anyhow::Result<u64> {
 }
 
 fn parse(input: &str) -> anyhow::Result<Map> {
-    let line_width = input
-        .lines()
-        .next()
-        .ok_or(AocError::InvalidInput)?
-        .chars()
-        .count();
-
-    let mut lines = 0;
-    let mut data = Vec::with_capacity(line_width * line_width);
+    let mut tiles = HashSet::new();
     let mut start = Coordinates::zeros();
-    let mut end = Coordinates::zeros();
     for (y, line) in input.lines().enumerate() {
-        lines += 1;
-
         for (x, c) in line.chars().enumerate() {
             let coordinates = Coordinates::new(x as u32, y as u32);
 
             match c {
-                '#' => data.push(Tile::Wall),
-                '.' => data.push(Tile::Path(-1)),
+                '.' | 'E' => {
+                    tiles.insert(coordinates);
+                }
                 'S' => {
-                    data.push(Tile::Path(-1));
+                    tiles.insert(coordinates);
                     start = coordinates;
                 }
-                'E' => {
-                    data.push(Tile::Path(-1));
-                    end = coordinates;
-                }
+                '#' => {}
                 _ => return Err(AocError::InvalidInput.into()),
             }
         }
     }
 
-    let grid = Grid::from_vec(line_width as u32, lines, data);
-    let map = Map::new(grid, start, end);
+    let map = Map::new(tiles, start);
 
     Ok(map)
 }
 
 #[derive(Debug, Clone)]
 struct Map {
-    grid: Grid<Tile>,
-    path: Vec<Coordinates>,
-    start: Coordinates,
-    #[allow(unused)]
-    end: Coordinates,
+    tiles: HashMap<Coordinates, i64>,
 }
 
 impl Map {
-    pub fn new(grid: Grid<Tile>, start: Coordinates, end: Coordinates) -> Self {
+    pub fn new(tiles: HashSet<Coordinates>, start: Coordinates) -> Self {
         let mut new = Self {
-            grid,
-            path: Vec::new(),
-            start,
-            end,
+            tiles: HashMap::with_capacity(tiles.len()),
         };
 
-        new.flood_fill_path();
-        new
-    }
-
-    fn flood_fill_path(&mut self) {
         let mut stack = Vec::new();
-        stack.push((self.start, 0));
+        stack.push((start, 0));
 
         while let Some((node, distance)) = stack.pop() {
-            match &mut self.grid[node] {
-                Tile::Path(d) if *d >= 0 => continue,
-                Tile::Path(d) => {
-                    *d = distance;
-                    self.path.push(node);
-                }
-                _ => unreachable!(),
-            }
+            new.tiles.insert(node, distance);
 
             for neighbor in CardinalNeighbors::new(node) {
-                if self
-                    .grid
-                    .get(neighbor)
-                    .is_some_and(|tile| matches!(tile, Tile::Path(d) if *d < 0))
-                {
+                if tiles.contains(&neighbor) && !new.tiles.contains_key(&neighbor) {
                     stack.push((neighbor, distance + 1))
                 }
             }
         }
-    }
-}
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Tile {
-    Path(i64),
-    Wall,
+        new
+    }
 }
 
 #[cfg(test)]
